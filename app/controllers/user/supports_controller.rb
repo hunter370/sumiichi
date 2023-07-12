ce04e4c628b0d061bc384cead6576727b323e9f9
EC2 Default User <ec2-user@ip-172-31-16-150.ap-northeast-1.compute.internal>
2023-07-12 12:46:14 +0000

    [Edit] user/support_display number limit

--- app/controllers/user/supports_controller.rb
+++ app/controllers/user/supports_controller.rb
@@ -1,30 +1,30 @@
 class User::SupportsController < ApplicationController
   def index
   end
-  
+
   def alps_first
     @game = Game.find(params[:id])
     @support = Support.new
-    @supports = Support.all
+    @supports = Support.where.not(alps_first: "").order(id: "DESC").limit(5)
   end
-  
+
   def alps_third
     @game = Game.find(params[:id])
     @support = Support.new
-    @supports = Support.all
+    @supports = Support.where.not(alps_third: "").order(id: "DESC").limit(5)
   end
-  
+
   def stand
     @game = Game.find(params[:id])
   end
-  
+
   def create
     @support = Support.new(support_params)
     @support.user_id = current_user.id
     @support.save!
-     redirect_to games_path
+    redirect_back(fallback_location: root_path)
   end
-  
+
   private
   def support_params
     params.require(:support).permit(:game_id, :alps_first, :alps_third, :stand)
