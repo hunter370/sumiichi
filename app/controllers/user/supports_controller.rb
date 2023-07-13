19274b29b86840b7cc67b2b2fd9a41527bf7b0f2
EC2 Default User <ec2-user@ip-172-31-16-150.ap-northeast-1.compute.internal>
2023-07-13 04:03:19 +0000

    [Edit] user/support_layout_third,stand

--- app/controllers/user/supports_controller.rb
+++ app/controllers/user/supports_controller.rb
@@ -5,17 +5,19 @@ class User::SupportsController < ApplicationController
   def alps_first
     @game = Game.find(params[:id])
     @support = Support.new
-    @supports = Support.where.not(alps_first: "").order(id: "DESC").limit(5)
+    @supports = Support.where.not(alps_first: "").order(id: "DESC").limit(20)
   end
 
   def alps_third
     @game = Game.find(params[:id])
     @support = Support.new
-    @supports = Support.where.not(alps_third: "").order(id: "DESC").limit(5)
+    @supports = Support.where.not(alps_third: "").order(id: "DESC").limit(20)
   end
 
   def stand
     @game = Game.find(params[:id])
+    @support = Support.new
+    @supports = Support.where.not(stand: "").order(id: "DESC").limit(20)
   end
 
   def create
